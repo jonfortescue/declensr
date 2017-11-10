9b5d9fe2569e61d731941b666f34ea2762b93a4e
Jon Fortescue <jonfortescue@protonmail.com>
2017-11-09 23:17:57 -0800

    Added lots of beginning functionality, including the beginning of language creation from a defined grammar file.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import *
+ # -*- coding: utf-8 -*-
+from flask import Flask, render_template, request
 from flask_pymongo import PyMongo
 import sys
+import re
 
 # without this, encoding issues cause errors
 reload(sys)
@@ -12,4 +14,52 @@ mongo = PyMongo(app) # establish MongoDB connection
 # index/home page
 @app.route("/")
 def home():
-    return render_template(title="Hello darkness my old friend")
+    return render_template("index.html", title="Which language are you studying?")
+
+@app.route("/lang/<lang>", methods=['GET', 'POST'])
+def lang(lang):
+    if request.method == 'GET':
+        if mongo.db[lang].count() == 0:
+            return render_template("create.html", title=lang)
+        else:
+            return "This language has definitions but this program isn't done."
+    elif request.method == 'POST':
+        data = request.form['langDef'].split('\n')
+        display = ""
+        prevIndent = 0
+        breadcrumbs = list()
+        bcop = list()
+        for line in data:
+            if line.strip() == "":
+                continue
+            pair = re.split(r':\s*', line)
+            indent = pair[0].count('  ')
+            key = pair[0].strip()
+            if len(pair) > 1:
+                value = pair[1].strip()
+
+            if len(breadcrumbs) == 0:
+                breadcrumbs.append(key)
+            elif indent == prevIndent:
+                breadcrumbs.pop()
+                breadcrumbs.append(key)
+            elif indent > prevIndent:
+                breadcrumbs.append(key)
+            elif indent < prevIndent:
+                assert (prevIndent - indent + 1) <= len(breadcrumbs)
+                for i in range(prevIndent - indent + 1):
+                    breadcrumbs.pop()
+                breadcrumbs.append(key)
+
+            bcop.append(' -> '.join(breadcrumbs))
+
+            if indent == 0:
+                if key == "Language":
+                    if value != lang:
+                        return "This grammar (%s) is not for the selected language (%s)." % (value, lang)
+                elif key == "Display":
+                    display = value
+            prevIndent = indent
+        return render_template("creation-report.html", title=display, bcop='\n'.join(bcop))
+    else:
+        return "HTTP 405 Method Not Allowed"
